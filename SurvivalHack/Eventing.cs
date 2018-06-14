87e3df8eaae33c08da72798e35c9d4d06251af20
wokste <git@stevenw.nl>
2018-06-14 08:59:52 +0200

    Moved Events to Eventing.cs

--- SurvivalHack/Eventing.cs
+++ SurvivalHack/Eventing.cs
@@ -1,4 +1,5 @@
-﻿using SurvivalHack.ECM;
+﻿using SurvivalHack.Combat;
+using SurvivalHack.ECM;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -42,6 +43,70 @@ namespace SurvivalHack
             }
 
             return true;
+        }
+    }
+
+    public class BaseEvent
+    {
+        public Entity Self;
+        public Entity Item;
+        public Entity Target;
+    }
+
+    public class DrinkEvent : BaseEvent
+    {
+        public DrinkEvent(Entity self, Entity item)
+        {
+            Self = self;
+            Item = item;
+        }
+    }
+
+    public class CastEvent : BaseEvent
+    {
+        public CastEvent(Entity self, Entity item)
+        {
+            Self = self;
+            Item = item;
+        }
+    }
+
+    public class ThreatenEvent : BaseEvent
+    {
+        public ThreatenEvent(Entity self, Entity target)
+        {
+            Self = self;
+            Target = target;
+        }
+    }
+
+    public class AttackEvent : BaseEvent
+    {
+        public EAttackState State = EAttackState.Hit;
+        public EAttackMove Move;
+
+        public AttackEvent(Entity self, Entity weapon, Entity target, EAttackMove move)
+        {
+            Self = self;
+            Item = weapon;
+            Target = target;
+            Move = move;
+        }
+    }
+
+    public class DamageEvent : BaseEvent
+    {
+        public int Damage;
+        public EDamageType DamageType;
+        public bool Significant => (Damage > 0);
+
+        public DamageEvent(BaseEvent prevMessage, int damage, EDamageType damageType)
+        {
+            Self = prevMessage.Self;
+            Item = prevMessage.Item;
+            Target = prevMessage.Target;
+            Damage = damage;
+            DamageType = damageType;
         }
     }
 }
