e8f230ac57f4ce43b9e18142b939cc6a345aa7ff
Wokste Laptop <gitlaptop@stevenw.nl>
2018-08-04 11:48:08 +0200

    Stuff

--- SurvivalHack/Mapgen/DungeonPopulator.cs
+++ SurvivalHack/Mapgen/DungeonPopulator.cs
@@ -9,7 +9,6 @@ namespace SurvivalHack.Mapgen
 {
     public class DungeonPopulator
     {
-        //private Dictionary<int, RandomTable<string>> _randomTables = new Dictionary<int, RandomTable<string>>();
         private readonly DungeonGenerator _gen;
         private EntityGenerationInfo _info;
         private Level _level;
