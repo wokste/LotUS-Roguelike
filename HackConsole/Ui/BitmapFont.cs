7ec7b3c82d9a7132e46dda20c7df35e361a30f45
Wokste Laptop <gitlaptop@stevenw.nl>
2018-08-18 19:43:33 +0200

    Increased font-size. Made things more configurable

--- HackConsole/Ui/BitmapFont.cs
+++ HackConsole/Ui/BitmapFont.cs
@@ -26,11 +26,18 @@ namespace HackConsole.Ui
 
         public Texture Texture;
         public Size Size;
+        public readonly int LineHeight;
+        public readonly int SpacingH;
+        public readonly int SpacingV;
 
         private Dictionary<char, BfChar> _charData = new Dictionary<char, BfChar>();
 
-        public BitmapFont(string name)
+        public BitmapFont(string name, int lineHeight, int spacingH, int spacingV)
         {
+            LineHeight = lineHeight;
+            SpacingH = spacingH;
+            SpacingV = spacingV;
+
             var image = new Image($"{name}.png");
             Texture = new Texture(image);
 
@@ -41,10 +48,10 @@ namespace HackConsole.Ui
                 while (!reader.EndOfStream)
                 {
                     var line = reader.ReadLine();
-                    var values = line.Split(',');
+                    var values = line.Split('\t');
                     Debug.Assert(values.Length == 4);
 
-                    var c = values[0] == "%COMMA%" ? ',' :  (values[0] == "%WTF%" ? '\0' :  values[0][0]);
+                    var c = (values[0] == "\\0" ? '\0' :  values[0][0]);
 
                     _charData[values[0][0]] = new BfChar(Int32.Parse(values[1]), Int32.Parse(values[2]), Int32.Parse(values[3]));
                 }
@@ -53,9 +60,9 @@ namespace HackConsole.Ui
             Debug.WriteLine(_charData.Count);
         }
 
-        public void Print(VertexArray vertexArray, string text, Rect r)
+        public void Print(VertexArray vertexArray, string text, int width, Vec v)
         {
-            Vector2f posF = new Vector2f(r.Left, r.Top);
+            Vector2f posF = new Vector2f(v.X, v.Y);
             foreach (var c in text)
             {
                 BfChar bitmapChar;
@@ -64,21 +71,22 @@ namespace HackConsole.Ui
                     throw new Exception();
 
                 PrintChar(vertexArray, bitmapChar, posF);
-                posF.X += bitmapChar.Width;
+                posF.X += bitmapChar.Width + SpacingH;
             }
         }
 
         private void PrintChar(VertexArray vertexArray, BfChar c, Vector2f posF)
         {
             var topColor = new SFML.Graphics.Color(255, 255, 255, 255);
-            var bottomColor = new SFML.Graphics.Color(255, 128, 128, 255);
+            var bottomColor = new SFML.Graphics.Color(255, 0, 0, 255);
             var texCoord = new Vector2f(c.Left, c.Top);
             var vX = new Vector2f(c.Width, 0);
-            var vY = new Vector2f(0, 12); // TODO: 12 is a magic number indicating font height.
+            var vY = new Vector2f(0, LineHeight); // TODO: 12 is a magic number indicating font height.
+            var vXY = vX + vY;
 
             vertexArray.Append(new Vertex(posF, topColor, texCoord));
             vertexArray.Append(new Vertex(posF + vX, topColor, texCoord + vX));
-            vertexArray.Append(new Vertex(posF + vX + vY, bottomColor, texCoord + vX + vY));
+            vertexArray.Append(new Vertex(posF + vXY, bottomColor, texCoord + vXY));
             vertexArray.Append(new Vertex(posF + vY, bottomColor, texCoord + vY));
         }
     }
--- HackConsole/Ui/Sprites.cs
+++ HackConsole/Ui/Sprites.cs
@@ -11,7 +11,7 @@ namespace HackConsole.Ui
     {
         public static Texture Ascii = MakeSprite("ascii.png");
         public static Texture Tileset = MakeSprite("tileset.png");
-        public static BitmapFont Font = new BitmapFont("font");
+        public static BitmapFont Font = new BitmapFont("font2",18,2,3);
 
         private static Texture MakeSprite(string texName)
         {
